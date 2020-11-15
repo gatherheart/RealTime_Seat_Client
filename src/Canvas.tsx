03136a0f268ea01419c8019956773e18a1ea46b2
gatherheart <imonit@naver.com>
2020-11-15 16:47:55 +0900

    [ADD] slot states and fixed rendering order

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,11 +1,16 @@
-import React, { ReactElement, useEffect, useRef } from 'react'
+import React, { ReactElement, useEffect, useRef, useState } from 'react'
 
 interface CanvasProps {
   text: string
 }
 
+interface ISlot {
+  opacity: number
+}
+
 const Canvas: React.FC<CanvasProps> = ({ text }: CanvasProps): ReactElement => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [slotStates, setSlotStates] = useState<Array<ISlot>>([])
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -15,10 +20,9 @@ const Canvas: React.FC<CanvasProps> = ({ text }: CanvasProps): ReactElement => {
 
     if (img && canvas && ctx) {
       img.onload = () => {
-        const [imgWidth, imgHeight, slotSize] = fitSizesIntoCanvas(img, canvas)
+        const [imgWidth, imgHeight] = fitSizesIntoCanvas(img, canvas)
         ctx.drawImage(img, 0, 0, imgWidth, imgHeight)
-        //ctx.fillStyle = 'red'
-        //ctx.fillRect(558.0 / 2, 377.0 / 2, slotSize, slotSize)
+        setSlotStates((prev) => [...prev, { opacity: 1 }])
       }
     }
   }, [])
@@ -33,46 +37,48 @@ const Canvas: React.FC<CanvasProps> = ({ text }: CanvasProps): ReactElement => {
         height={CANVAS_SIZE}
         style={{ position: 'absolute', borderColor: 'white', borderWidth: 1, top: 0, left: 0 }}
       >
-        <rect
-          x={`${558.0 / 2}`}
-          y={`${377.0 / 2}`}
-          width="5.0"
-          height="5.0"
-          className="seat-normal"
-          data-center-x="563"
-          data-center-y="382"
-          data-radius="7.0710678118654755"
-          data-id="28253340"
-          id="28253340"
-          data-group="13"
-          style={{ fill: 'red', stroke: 'black', strokeWidth: 0, opacity: 0.5 }}
-        ></rect>
+        {slotStates.length !== 0
+          ? slotStates.map((slot) => {
+              return (
+                <rect
+                  x={`${558.0 / 2}`}
+                  y={`${377.0 / 2}`}
+                  width="5.0"
+                  height="5.0"
+                  className="seat-normal"
+                  data-center-x="563"
+                  data-center-y="382"
+                  data-radius="7.0710678118654755"
+                  data-id="28253340"
+                  id="28253340"
+                  data-group="13"
+                  style={{ fill: 'red', stroke: 'black', strokeWidth: 0, opacity: slot.opacity }}
+                ></rect>
+              )
+            })
+          : null}
       </svg>
     </div>
   )
 }
 
 const CANVAS_SIZE = 657
-const SLOT_SIZE = 10
 const background = 'https://naverbooking-phinf.pstatic.net/20201007_220/1602053055804OStiU_PNG/image.png'
 
 const fitSizesIntoCanvas = (img: HTMLImageElement, canvas: HTMLCanvasElement): Array<number> => {
   const imgRatio = img.height / img.width
   const canvasRatio = canvas.height / canvas.width
   let imageHeight = 0,
-    imageWidth = 0,
-    slotSize = 0
+    imageWidth = 0
 
   if (imgRatio < canvasRatio) {
     imageWidth = canvas.width
     imageHeight = imageWidth * imgRatio
-    slotSize = Math.floor(SLOT_SIZE / (img.width / CANVAS_SIZE))
   } else {
     imageHeight = canvas.height
     imageWidth = imageHeight / imgRatio
-    slotSize = Math.floor(SLOT_SIZE / (img.height / CANVAS_SIZE))
   }
-  return [imageWidth, imageHeight, slotSize]
+  return [imageWidth, imageHeight]
 }
 
 export default Canvas
