8c44013436cba2dfcee5ff7eaff396d3293eac84
gatherheart <imonit@naver.com>
2020-11-15 15:13:36 +0900

    [ADD] Background image in canvas

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -4,32 +4,46 @@ interface CanvasProps {
   text: string
 }
 
-const cheese =
-  'https://images.unsplash.com/photo-1519411792752-25c2468cccb3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=354&q=80'
 const Canvas: React.FC<CanvasProps> = ({ text }: CanvasProps): ReactElement => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const imgRef = useRef<HTMLImageElement>(null)
 
-  console.log('CANVAS')
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas?.getContext('2d')
-    const img = imgRef.current
-    if (img && ctx) {
+    const img = new Image()
+    img.src = cheese
+
+    if (img && canvas && ctx) {
       img.onload = () => {
-        ctx.drawImage(img, 20, 20)
-        ctx.font = '40px Courier'
-        ctx.fillText(text, 210, 75)
+        const [imgWidth, imgHeight] = fitImageIntoCanvas(img, canvas)
+        ctx.drawImage(img, 0, 0, imgWidth, imgHeight)
       }
     }
   }, [])
 
   return (
     <div>
-      <canvas ref={canvasRef} width={640} height={425} />
-      <img ref={imgRef} src={cheese} className="hidden" />
+      <canvas ref={canvasRef} width={CANVAS_SIZE} height={CANVAS_SIZE} />
     </div>
   )
+}
+
+const CANVAS_SIZE = 657
+const cheese = 'https://naverbooking-phinf.pstatic.net/20201007_220/1602053055804OStiU_PNG/image.png'
+const fitImageIntoCanvas = (img: HTMLImageElement, canvas: HTMLCanvasElement): Array<number> => {
+  const imgRatio = img.height / img.width
+  const canvasRatio = canvas.height / canvas.width
+  let imageHeight = 0,
+    imageWidth = 0
+
+  if (imgRatio < canvasRatio) {
+    imageWidth = canvas.width
+    imageHeight = imageWidth * imgRatio
+  } else {
+    imageHeight = canvas.height
+    imageWidth = imageHeight / imgRatio
+  }
+  return [imageWidth, imageHeight]
 }
 
 export default Canvas
