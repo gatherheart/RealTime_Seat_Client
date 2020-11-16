5ac0fbcfb98877e9e72bb64190ac625afe64a586
gatherheart <imonit@naver.com>
2020-11-16 12:01:51 +0900

    [ADD] RC added and method to access target id&tagName

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -4,6 +4,10 @@ interface CanvasProps {
   text: string
 }
 
+interface SeatProps {
+  slot: ISlot
+}
+
 interface ISlot {
   id: number
   cord: Point
@@ -43,32 +47,21 @@ const Canvas: React.FC<CanvasProps> = ({ text }: CanvasProps): ReactElement => {
       <canvas ref={canvasRef} width={CANVAS_SIZE} height={CANVAS_SIZE}></canvas>
 
       <svg
+        id={'123213'}
         xmlns="http://www.w3.org/2000/svg"
         width={CANVAS_SIZE}
         height={CANVAS_SIZE}
         style={{ position: 'absolute', borderColor: 'white', borderWidth: 1, top: 0, left: 0 }}
         onClick={(event) => {
-          console.log(event.target)
+          console.dir(event.target)
+          console.log(event.target as HTMLInputElement)
+          console.log((event.target as HTMLInputElement).id)
+          console.log((event.target as HTMLInputElement).tagName)
         }}
       >
         {slotStates.length !== 0
           ? slotStates.map((slot, idx) => {
-              return (
-                <rect
-                  id={`${slot.id}`}
-                  x={`${slot.cord.x}`}
-                  y={`${slot.cord.y}`}
-                  width="5.0"
-                  height="5.0"
-                  className="seat-normal"
-                  data-center-x="563"
-                  data-center-y="382"
-                  data-radius="7.0710678118654755"
-                  data-id="28253340"
-                  data-group="13"
-                  style={{ fill: 'red', stroke: 'black', strokeWidth: 0, opacity: slot.opacity }}
-                ></rect>
-              )
+              return <Seat slot={slot}></Seat>
             })
           : null}
       </svg>
@@ -78,6 +71,25 @@ const Canvas: React.FC<CanvasProps> = ({ text }: CanvasProps): ReactElement => {
 
 const CANVAS_SIZE = 657
 const background = 'https://naverbooking-phinf.pstatic.net/20201007_220/1602053055804OStiU_PNG/image.png'
+
+const Seat: React.FC<SeatProps> = ({ slot }: SeatProps): ReactElement => {
+  return (
+    <rect
+      id={`${slot.id}`}
+      x={`${slot.cord.x}`}
+      y={`${slot.cord.y}`}
+      width="5.0"
+      height="5.0"
+      className="seat-normal"
+      data-center-x="563"
+      data-center-y="382"
+      data-radius="7.0710678118654755"
+      data-id="28253340"
+      data-group="13"
+      style={{ fill: 'red', stroke: 'black', strokeWidth: 0, opacity: slot.opacity }}
+    ></rect>
+  )
+}
 
 const fitSizesIntoCanvas = (img: HTMLImageElement, canvas: HTMLCanvasElement): Array<number> => {
   const imgRatio = img.height / img.width
